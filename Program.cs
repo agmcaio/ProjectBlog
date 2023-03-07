aa9a3790205767d7b0c08a095bd88c351f90bf25
Caio André Guilherme de Miranda <caio.miranda@vsoft.com.br>
2023-03-06 22:11:35 -0300

    Cursando

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using Dapper.Contrib.Extensions;
+using DapperProjectBlog.Models;
+using DapperProjectBlog.Repositories;
+using Microsoft.Data.SqlClient;
+
+namespace DapperProjectBlog
+{
+    class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=blog-project;User Id=sa;Password=santos2002;TrustServerCertificate=True;";
+
+        static void Main(string[] args)
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                ReadUsers(connection);
+                ReadRoles(connection);
+            }
+        }
+
+        // USER
+        static void ReadUsers(SqlConnection connection)
+        {
+            var repository = new UserRepository(connection);
+            var users = repository.Get();
+
+            foreach (var user in users)
+                Console.WriteLine(user.Name);
+        }
+
+        static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        static void CreateUser()
+        {
+            var user = new User()
+            {
+                // O id já é gerado;
+                Bio = "Estudante de Tecnologia",
+                Email = "igor@email.com",
+                Image = "https://",
+                Name = "Igor",
+                PasswordHash = "HASH",
+                Slug = "agmigor"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado!");
+            }
+        }
+   
+        static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Bio = "Desenvolvedor",
+                Email = "igor@email.com",
+                Image = "https://",
+                Name = "Igor",
+                PasswordHash = "HASH",
+                Slug = "agmigor"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+                Console.WriteLine("Atualização Realizada!");
+            }
+        }
+    
+        static void DeleteUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(2); // Obtendo o usuário
+                connection.Delete<User>(user); // Apagando usuário
+                Console.WriteLine("Remoção Realizada!");
+            }
+        }
+
+        // ROLE
+        static void ReadRoles(SqlConnection connection)
+        {
+            var repository = new RoleRepository(connection);
+            var roles = repository.Get();
+
+            foreach (var role in roles)
+                Console.WriteLine(role.Name);
+        }
+    }
+
+}
---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using Dapper.Contrib.Extensions;
+using DapperProjectBlog.Models;
+using Microsoft.Data.SqlClient;
+
+namespace DapperProjectBlog.Repositories
+{
+    public class UserRepository
+    {
+        private readonly SqlConnection _connection;
+
+        public UserRepository(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<User> Get()
+        {
+            return _connection.GetAll<User>();
+        }
+
+        public User Get(int id)
+        {
+            return _connection.Get<User>(id);
+        }
+
+        public void Insert(User user)
+        {
+            user.Id = 0; // Pra não ocorrer de colocar um id que já exista
+            _connection.Insert<User>(user);
+        }
+
+        public void Update(User user)
+        {
+            if (user.Id != 0) 
+            {
+                _connection.Insert<User>(user);
+            }
+        }
+
+        public void Delete(User user)
+        {
+            if (user.Id != 0)
+            {
+                _connection.Delete<User>(user);
+            }
+        }
+
+        public void Delete(int id)
+        {
+            if (id != 0)
+                return; // sair da função
+
+            var user = _connection.Get<User>(id);
+            _connection.Delete<User>(user);
+        }
+    }
+}
