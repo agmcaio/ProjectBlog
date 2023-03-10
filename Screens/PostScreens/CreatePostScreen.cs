be82f49d292e42b1f104d84254b64674e2fd7436
Caio André Guilherme de Miranda <caio.miranda@vsoft.com.br>
2023-03-10 00:34:07 -0300

    Atualização

---
+++ Screens/PostScreens/CreatePostScreen.cs
@@ -0,0 +1,39 @@
+﻿using DapperProjectBlog.Models;
+
+namespace DapperProjectBlog.Screens.PostScreens
+{
+    public static class CreatePostScreen
+    {
+        public static void Load()
+        {
+            Console.Clear();
+            Console.WriteLine("ENTER YOUR DATA FOR POST:");
+            Create();
+        }
+
+        public static void Create() 
+        {
+            var post = new Post();
+
+            Console.WriteLine("Category ID:");
+            post.CategoryId
+
+            Console.WriteLine("Title:");
+            post.Title = Console.ReadLine();
+
+            Console.WriteLine("Summary:");
+            post.Summary = Console.ReadLine();
+            
+            Console.WriteLine("Body:");
+            post.Body = Console.ReadLine();
+            
+            Console.WriteLine("Slug:");
+            post.Slug = Console.ReadLine();
+
+            post.CreateDate = DateTime.Now;
+            post.LastUpdateDate = DateTime.Now;
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Screens/PostScreens/MenuPostScreen.cs
@@ -0,0 +1,49 @@
+﻿using DapperProjectBlog.Screens.UserScreen;
+using DapperProjectBlog.Screens.UserScreens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperProjectBlog.Screens.PostScreens
+{
+    public static class MenuPostScreen
+    {
+        public static void Load()
+        {
+            Console.Clear();
+            Console.WriteLine("Post Management");
+            Console.WriteLine();
+            Console.WriteLine("1 - Register post");
+            Console.WriteLine("2 - Update post");
+            Console.WriteLine("3 - list especif post");
+            Console.WriteLine("4 - List all posts");
+            Console.WriteLine("5 - Delete post");
+            Console.WriteLine("0 - Back");
+            Console.WriteLine();
+            var option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+                    CreatePostScreen.Load();
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 0:
+                    break;
+                default:
+                    Console.WriteLine("Option Invalid!");
+                    Console.ReadLine();
+                    Load();
+                    break;
+            }
+    }
+}
